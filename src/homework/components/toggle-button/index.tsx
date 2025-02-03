7fd27cf7bb18e00437e9a8162f04963811234226
betteree <kkne1017@gmail.com>
2025-02-03 20:03:42 +0900

    스타일링링

--- src/homework/components/toggle-button/index.tsx
+++ src/homework/components/toggle-button/index.tsx
@@ -1,3 +1,5 @@
+import { tm } from '@/utils/tw-merge';
+
 type ToggleButtonProps = React.ComponentProps<'button'> & {
   label: string;
   isOff?: boolean;
@@ -13,11 +15,14 @@ function ToggleButton({
   className = '',
   ...buttonProps
 }: ToggleButtonProps) {
-  const buttonStyles = `
-    flex justify-center items-center 
-    size-8 rounded-md border-0 p-0 bg-transparent
-    leading-none hover:not-[:disabled,[aria-disabled=true]]:bg-black/10
-  ${className}`;
+  const buttonStyles = tm(
+    `flex justify-center items-center`,
+    `size-8 rounded-md border-0 px-0 py-0 bg-transparent`,
+    `leading-none`,
+    `hover:not-[:disabled,[aria-disabled=true]]:bg-black/10`,
+    // { 'bg-sky-300': !isOff },
+    className
+  );
 
   return (
     <button className={buttonStyles} {...buttonProps}>
