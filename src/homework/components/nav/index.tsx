6bc7aa23bbfc7ed069b73e9ba49e1291ad69539e
betteree <kkne1017@gmail.com>
2025-02-04 10:42:23 +0900

    실습

--- src/homework/components/nav/index.tsx
+++ src/homework/components/nav/index.tsx
@@ -1,6 +1,6 @@
-import clsx from 'clsx/lite';
 import { useState } from 'react';
 import { getUIView, type UIView } from '@/homework/lib/ui-view';
+import NavLink from './nav-link';
 import './style.css';
 
 function Nav() {
@@ -12,30 +12,23 @@ function Nav() {
   const isSignUpView = uiView.includes('signup');
   const isStateManagementView = uiView.includes('state-management');
 
+  // 내비게이션 리스트
+  const navList = [
+    { id: 'item-1', content: '로그인', href: 'signin', isActive: isSignInView },
+    { id: 'item-2', content: '회원가입', href: 'signup', isActive: isSignUpView },
+    { id: 'item-3', content: '상태 관리 전략', href: 'state-management', isActive: isStateManagementView },
+  ];
+
   return (
     <nav className="nav">
       <h2 className="sr-only">페이지 탐색</h2>
-      <a
-        href="/?view=signin"
-        className={clsx(isSignInView && 'active')}
-        aria-current={isSignInView ? 'page' : undefined}
-      >
-        로그인
-      </a>
-      <a
-        href="/?view=signup"
-        className={clsx(isSignUpView && 'active')}
-        aria-current={isSignUpView ? 'page' : undefined}
-      >
-        회원가입
-      </a>
-      <a
-        href="/?view=state-management"
-        className={clsx(isStateManagementView && 'active')}
-        aria-current={isStateManagementView ? 'page' : undefined}
-      >
-        상태 관리 전략
-      </a>
+      {
+        navList.map((navItem) => (
+          <NavLink key={navItem.id} href={navItem.href} isActive={navItem.isActive}>
+            {navItem.content}
+          </NavLink>
+        ))
+      }
     </nav>
   );
 }
---
+++ src/homework/pages/state-management/components/accordion-opened-count.tsx
@@ -0,0 +1,3 @@
+export function AccordionOpenedCount() {
+  return <output className="text-6xl font-normal text-slate-700">0</output>;
+}
