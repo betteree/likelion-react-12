2007763627c458a03d815e72fd748761c342889a
betteree <kkne1017@gmail.com>
2025-02-04 17:13:50 +0900

    틱택토

--- src/homework/components/nav.tsx
+++ src/homework/components/nav.tsx
@@ -6,6 +6,8 @@ function Nav() {
       <h2 className="sr-only">페이지 탐색</h2>
       <a href="/?view=signin">로그인</a>
       <a href="/?view=signup">회원가입</a>
+      <a href="/?view=state-management">상태관리</a>
+      <a href="/?view=tic-tac-toe">틱택토</a>
     </nav>
   );
 }
