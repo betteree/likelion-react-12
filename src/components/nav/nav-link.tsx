31358f5573f2916d28c066af2eac17a76bc35563
betteree <kkne1017@gmail.com>
2025-02-07 11:49:50 +0900

    [실습] 클래스 컴포넌트 라이프 사이클 메서드 2
    - componentWillUnmount
    - [사이드 이펙트] 이벤트 구독/취소
    - 정리의 중요성
    - 메모리 누수 방지지

--- src/components/nav/nav-link.tsx
+++ src/components/nav/nav-link.tsx
@@ -1,5 +1,5 @@
-import { getView, setView } from '@/router/manage-view';
 import { tm } from '@/utils/tw-merge';
+import { getView, setView } from '@/router/manage-view';
 
 type WithoutHref = Omit<React.ComponentProps<'a'>, 'href'>;
 type OnChangeRoute = React.Dispatch<React.SetStateAction<string>>;
@@ -22,8 +22,11 @@ function NavLink({
   const isActive = view.includes(href);
 
   const handleRoute = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    // 네이티브 이벤트 시스템 중단
     e.preventDefault();
+    // 프로그래밍 방식 내비게이션 설정
     onChangeRoute(href);
+    // Playground 컴포넌트의 상태 업데이트 요청
     setView(href);
   };
 
