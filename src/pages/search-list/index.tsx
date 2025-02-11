69d6589a984349a33cfea502a68e8432babde851
betteree <kkne1017@gmail.com>
2025-02-11 15:43:00 +0900

    실시간 검색색

--- src/pages/search-list/index.tsx
+++ src/pages/search-list/index.tsx
@@ -1,17 +1,25 @@
 import { useState } from 'react';
+import { tm } from '@/utils/tw-merge';
 import SearchForm from './components/search-form';
 import SearchedList from './components/searched-list';
 import colorMoodList from './data/color-mood-list';
 import { type ColorMoodItem } from './types';
 
 function SearchListPage() {
-  const [list] = useState<ColorMoodItem[]>(colorMoodList);
+  const [list, setList] = useState<ColorMoodItem[]>(colorMoodList);
+  const [query, setQuery] = useState('');
+
+  const handleUpdateList = (item: ColorMoodItem, isFavorited: boolean) => {
+    setList(
+      list.map((it) => (it.id === item.id ? { ...it, isFavorited } : it))
+    );
+  };
 
   return (
-    <section>
+    <section className={tm('flex flex-col gap-5 items-center')}>
       <h2 className="text-2xl font-medium text-react">카드 검색 리스트</h2>
-      <SearchForm />
-      <SearchedList list={list} />
+      <SearchForm query={query} setQuery={setQuery} />
+      <SearchedList list={list} query={query} onUpdate={handleUpdateList} />
     </section>
   );
 }
