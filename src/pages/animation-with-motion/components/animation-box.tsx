a2e22279292d8ef8522dede8aba11ca779bcf10d
betteree <kkne1017@gmail.com>
2025-02-13 10:39:09 +0900

    딜레이 애니메이션

--- src/pages/animation-with-motion/components/animation-box.tsx
+++ src/pages/animation-with-motion/components/animation-box.tsx
@@ -17,8 +17,11 @@ function AnimationBox({
         boxElement,
         // 애니메이션 속성 (CSS 속성, JavaScript 객체 또는 배열 데이터, SVG, ...)
         {
-          x: 240 /* 24px 만큼 이동 */,
+          // x: 240 /* 24px 만큼 이동 */,
           rotate: -360 /* -360deg 만큼 회전 */,
+          scale: 0.55,
+          x: -28,
+          y: -32,
         },
         // 애니메이션 옵션
         {
@@ -37,7 +40,7 @@ function AnimationBox({
         'flex justify-center items-center',
         'size-30',
         'bg-react text-white text-lg font-medium',
-        'rounded-lg',
+        'rounded-xl',
         className
       )}
       {...restProps}
--- src/pages/animation-with-motion/index.tsx
+++ src/pages/animation-with-motion/index.tsx
@@ -1,19 +1,55 @@
-import { BookHome, Box } from '@mynaui/icons-react';
+import { useState } from 'react';
+import { tm } from '@/utils/tw-merge';
+import { Box, Redo } from '@mynaui/icons-react';
 import AnimationBox from './components/animation-box';
+import StaggerList from './components/stagger-list';
 
 function AnimationWithMotionPage() {
+  // 화면 업데이트를 위한 상태 선언
+  const [replayKey, setReplayKey] = useState(0);
+
+  // 상태 업데이트 감지해서 이펙트 처리
+  // useEffect(() => {
+  //   console.log({ replayKey });
+  // }, [replayKey]);
+
+  const handleReplay = () => {
+    // const nextReplayKey = replayKey + 1; // 0, 1, 2, 3, ...
+    // setReplayKey(nextReplayKey);
+    // setReplayKey((replayKey) => {
+    //   return replayKey + 1;
+    // });
+
+    // 리플레이 할 수도 있도록 키 증가
+    setReplayKey((r) => r + 1);
+  };
+
   return (
-    <section className="flex flex-col gap-7">
+    <section className="flex flex-col items-start gap-7">
       <h2 className="text-2xl font-medium">
         애니메이션 - Motion 라이브러리 활용
       </h2>
 
-      <AnimationBox>
-        <Box size={32} />
-      </AnimationBox>
+      <button
+        type="button"
+        onClick={handleReplay}
+        className={tm(
+          'cursor-pointer',
+          'flex items-center gap-1.5 px-3.5 py-2 rounded-lg',
+          'bg-react text-white text-sm font-semibold uppercase',
+          'active:scale-95'
+        )}
+      >
+        <Redo size={18} />
+        Replay
+      </button>
 
-      <AnimationBox>
-        <BookHome size={32} />
+      <StaggerList />
+
+      {/* key 속성이 변경되면, 리액트 컴포넌트 초기화 */}
+      {/* 초기화: 애니메이션을 다시 진행 */}
+      <AnimationBox key={replayKey}>
+        <Box size={48} />
       </AnimationBox>
     </section>
   );
