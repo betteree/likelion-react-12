94f72e37c653e1485b84c194a91db8e40f531e20
betteree <kkne1017@gmail.com>
2025-02-16 06:39:30 +0900

    설치 업데이트트

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
+import { reactClickToComponent } from 'vite-plugin-react-click-to-component';
 
 const viteConfig = defineConfig((env) => {
   const isDevMode = env.mode.includes('development');
@@ -11,17 +12,18 @@ const viteConfig = defineConfig((env) => {
     plugins: [
       react({
         jsxRuntime: 'automatic',
-        babel: {
-          plugins: [
-            [
-              '@locator/babel-jsx/dist',
-              {
-                env: 'development',
-              },
-            ],
-          ],
-        },
+        // babel: {
+        //   plugins: [
+        //     [
+        //       '@locator/babel-jsx/dist',
+        //       {
+        //         env: 'development',
+        //       },
+        //     ],
+        //   ],
+        // },
       }),
+      reactClickToComponent(),
       tailwindcss(),
     ],
     server: {
