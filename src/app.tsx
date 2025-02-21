a898b9e3a717d322fc1334f3c31a92453df90e90
betteree <kkne1017@gmail.com>
2025-02-21 14:39:15 +0900

    레이아웃웃

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,32 @@
+import { BrowserRouter, Route, Routes } from 'react-router';
 import { ErrorBoundary } from 'react-error-boundary';
-import PrintError from './components/error';
-import Playground from './playground';
 import { ThemeProvider, ThemeSetters } from './contexts/theme';
-// import { useCountStore } from './stores/count';
+import PlaygroundPage from './pages/playground/page';
+import SignInPage from './pages/sign-in/page';
+import SignUpPage from './pages/sign-up/page';
+import PrintError from './components/error';
+import HomePage from './pages/home/page';
+import AuthLayout from './layouts/auth/layout';
 
 function App() {
-  // 앱 상태 관리 (테마, 인증 : UI 전체 단위 공통 컴포넌트 제어)
-  // 1. 속성(데이터) 하위 컴포넌트에 전달 x N (prop drilling) 😱
-  // 2. Context API (컨텍스트 생성 후, 값을 공급) 🤔
-  // 3. 외부 상태 관리 라이브러리 활용 (Zustand, Redux or RTK, Jotai, ...)
-
-  // 컴포넌트 Zustand 스토어 훅 사용 바인딩
-  // const count = useCountStore(({ count }) => count);
-
   return (
     <ErrorBoundary FallbackComponent={PrintError}>
       <ThemeProvider>
         <ThemeSetters />
-        {/* <output className='block text-6xl pl-4 mt-4'>{count}</output> */}
-        <Playground />
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
+          <Routes>
+            <Route index element={<HomePage />} />
+            <Route path="/playground" element={<PlaygroundPage />} />
+
+            {/* 레이아웃 */}
+            <Route path="/auth" element={<AuthLayout />}>
+              {/* 중첩된 라우트(Nested Routes) */}
+              {/* 페이지는 배출구(Outlet)에서 렌더링 */}
+              <Route path="signin" element={<SignInPage />} />
+              <Route path="signup" element={<SignUpPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </ThemeProvider>
     </ErrorBoundary>
   );
